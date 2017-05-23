b6a4c7ed992f9b8e6831e220b224976e8e3f3aac
Vaibhav <geekvaibhav@users.noreply.github.com>
2017-05-23 20:15:48 +0530

    Token Update

--- config.js
+++ config.js
@@ -1,7 +1,11 @@
 module.exports = {
-    FB_PAGE_TOKEN: 'EAAcEl6o4PAwBAJhkjctnT8XrHbTdBPl0pkyxDQN4KZChFYbMgeQWdZAOU6ZCRKvsjqrTZA0NBEgZB1fmj98wA1pWpiZC6GpZBpwEUD0eXXETNZAIQTorTtimBdOeLi4RaQCZCJoddOAjWu0zpBTb3ZCh1zZCY0V4XPWs39EcPbFbf4LzAZDZD',
+    FB_PAGE_TOKEN: 'EAAEVvUWM28QBAEHXxv8dfuVQjDkf5fQOmtcIUkDA1CQpUlO3nsaWtd8EN4Q5sb6ef1X1TmiOZC5Gvx3ER2tyU0iA9pigCZBV58EAOyJ1bJnkumdRz1gOh4gCLx4ZCR0Yqr3ZAB2MKAv0tbnX3H3wFkQPjW4dH0viltV1zeZBPOAZDZD',
     FB_VERIFY_TOKEN: 'IndoreMeriJaan0731',
-    API_AI_CLIENT_ACCESS_TOKEN: 'a26c3ef325dd4758bbbf9930c2b58a8e',
-    FB_APP_SECRET: '5afb2220b74547dfd4ae84f443a44319',
-    SERVER_URL: "https://jigneshtravels.herokuapp.com/",
+    API_AI_CLIENT_ACCESS_TOKEN: 'a0608bd94bc04fe8bacab682d44fceea',
+    FB_APP_SECRET: '371c00861233ef4bb19cffb1c1a6bb4e',
+    SERVER_URL: "https://localhosttravelagent.herokuapp.com/",
+    SENGRID_API_KEY: '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',
+    EMAIL_FROM: 'jana@ucan-courses.com',
+    EMAIL_TO: 'vaibhav.singh144@gmail.com',
+    WEATHER_API_KEY: 'b687283f063884d31e0f527129779ef2',
 };
