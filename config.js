6cb5f00711ac58fd95fc771a10e82cfbbf5e120c
Vaibhav <geekvaibhav@users.noreply.github.com>
2017-05-23 19:36:49 +0530

    Token Update

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     FB_PAGE_TOKEN: 'EAAcEl6o4PAwBAJhkjctnT8XrHbTdBPl0pkyxDQN4KZChFYbMgeQWdZAOU6ZCRKvsjqrTZA0NBEgZB1fmj98wA1pWpiZC6GpZBpwEUD0eXXETNZAIQTorTtimBdOeLi4RaQCZCJoddOAjWu0zpBTb3ZCh1zZCY0V4XPWs39EcPbFbf4LzAZDZD',
     FB_VERIFY_TOKEN: 'IndoreMeriJaan0731',
-    API_AI_CLIENT_ACCESS_TOKEN: 'a0608bd94bc04fe8bacab682d44fceea',
+    API_AI_CLIENT_ACCESS_TOKEN: 'a26c3ef325dd4758bbbf9930c2b58a8e',
     FB_APP_SECRET: '5afb2220b74547dfd4ae84f443a44319',
     SERVER_URL: "https://jigneshtravels.herokuapp.com/",
 };
